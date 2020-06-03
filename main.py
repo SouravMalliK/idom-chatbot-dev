e44bde66c521fea943f78afdea70e6316ce62dc1
SouravMallik <Sourav.Mallik@outlook.com>
2020-06-03 21:15:36 +0530

    content list response added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import logging
 import os
+from libraries.df_response_lib import *
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask,request, make_response, jsonify
 from google.cloud import firestore
 
 app = Flask(__name__)
@@ -25,57 +26,84 @@ def webhook(request):
         return 'json error'
 
     if action == 'get.session.status':
-        res = sessionStatus(req)
+        aog = actions_on_google_response()
+        fulfillmentText, current_content, current_meaning = sessionStatus(req)
+
+        aog_sr = aog.simple_response([
+            [fulfillmentText, fulfillmentText, False]
+        ])
+
+        # aog_sc = aog.suggestion_chips(current_content.get(u'items'))
+        list_title, list_arr = get_content_list(current_content)
+        list_select = aog.list_select(list_title, list_arr)
+
+        ff_response = fulfillment_response()
+        ff_text = ff_response.fulfillment_text(fulfillmentText)
+        ff_messages = ff_response.fulfillment_messages([aog_sr, list_select])
+
+        reply = ff_response.main_response(ff_text, ff_messages)
+
+    elif action == 'input.welcome':
+        logging.info('welcome intent')
     else:
-        log.error('Unexpected action.')
+        logging.error('Unexpected action.')
 
-    print('Action: ' + action)
-    print('Response: ' + res)
+    return make_response(jsonify(reply))
 
-    return make_response(jsonify({'fulfillmentText': res}))
 
 
 def sessionStatus(req):
+    suggestion_list = []
     logging.info(req.get('queryResult').get('parameters'))
 
     result = req.get('queryResult')
     parameters = result.get('parameters')
     user = parameters.get('user-name')
 
-    # statusConst = {'sourav': True, 'shamik': False}
-
-    # if statusConst[user]:
-    #     speech = 'Ok ' + user + ', here is the list of idioms with the last one highlighted and performance of others displayed'
-    # else:
-    #     speech = 'Let me explain the objective, teaching and assessment methods'
-
     try:
-        is_session_started = lookup_session_status(user)
+        if not firebase_admin._apps:
+            db = setup_client()
+        db = firestore.client()
+        is_session_started, first_name = lookup_users(db, user)
     except Exception as e:
         logging.info(e)
+        exit(1)
 
     if is_session_started == True:
-        speech = 'Ok ' + user + ', here is the list of idioms with the last one highlighted and performance of others displayed'
+        speech = 'Ok ' + first_name + ', here is the list of idioms we are going to discuss in this session'
+        current_content, current_meaning = get_content(db,user)
+
     elif is_session_started == False:
-        speech = 'Ok ' + user + ', let me explain the objective, teaching and assessment methods.'
+        speech = 'Ok ' + first_name + ', let me explain the objective, teaching and assessment methods.'
     else:
-        speech = 'User ' + user + ' is not registered. Please register yourself, before starting this tutorials.'
+        speech = 'User ' + first_name + ' is not registered. Please register yourself, before starting this tutorials.'
 
     logging.info('Response: %s', speech)
-    return speech
+    return speech, current_content, current_meaning
 
+def get_content_list(current_content):
+    list_title = current_content.get(u'title')
+    list_arr = [
+        [current_content.get(u'title'), current_content.get(u'paragraphs').get(u'para_1'), ["item1", "item2"],
+         [current_content.get(u'images'), current_content.get(u'titles')]],
+        [current_content.get(u'title'), current_content.get(u'paragraphs').get(u'para_2'), ["item1", "item2"],
+         [current_content.get(u'images'), current_content.get(u'titles')]]
+    ]
+    return list_title, list_arr
 
-def lookup_session_status(user):
-    if not firebase_admin._apps:
-        db = setup_client()
+def get_content(db, user):
+    learning = (db.collection(u'learnings').document(user).get()).to_dict()
 
-    db = firestore.client()
-    docs = db.collection(u'users').where(u'first_name', u'==', user).stream()
-    result = {}
-    for doc in docs:
-        result = doc.to_dict()
+    content_dict = db.document(learning.get(u'content_id').path).get().to_dict()
+    meaning_dict = db.document(learning.get(u'learning_status').get('last_meaning_id').path).get().to_dict()
 
-    return result.get('session_started')
+    return content_dict, meaning_dict
+
+def lookup_users(db, user):
+    user_ref = (db.collection(u'users').document(user)).get()
+    result = user_ref.to_dict()
+
+    return result.get(u'session_started'), result.get(u'first_name')
 
 
 def setup_client():
@@ -86,6 +114,9 @@ def setup_client():
     })
 
     return firestore.client()
+
+def generate_uuid(doc_ref, id_col):
+    return doc_ref.update({ id_col : firestore.Increment(1)})
 
 
 if __name__ == '__main__':
