ddcd765bf1e58b05297b1c8ab3341012e2d5adf7
SouravMallik <Sourav.Mallik@outlook.com>
2020-06-17 17:51:18 +0530

    db updated

--- learnings.py
+++ learnings.py
@@ -1,57 +1,97 @@
 # [START custom_class_def]
 class Learning(object):
-    def __init__(self, user, content_id, meaning_id, question_id, introduction_status=False,
-                 story_status=False, meaning_status=False, example_status=False):
+    def __init__(self, user, content_id="", last_content_watched_on="", content_para_1=False, content_para_2=False,
+                 content_para_3=False, content_para_4=False,
+                 meaning_id="", last_meaning_watched_on="", meaning_para_1=False, meaning_para_2=False,
+                 meaning_para_3=False, meaning_para_4=False):
         self.user = user
         self.content_id = content_id
+        self.last_conent_watched_on = last_content_watched_on
+        self.content_para_1 = content_para_1
+        self.content_para_2 = content_para_2
+        self.content_para_3 = content_para_3
+        self.content_para_4 = content_para_4
         self.meaning_id = meaning_id
-        self.question_id = question_id
-        self.introduction_status = introduction_status
-        self.story_status = story_status
-        self.meaning_status = meaning_status
-        self.exaple_status = example_status
+        self.last_meaning_watched_on = last_meaning_watched_on
+        self.meaning_para_1 = meaning_para_1
+        self.meaning_para_2 = meaning_para_2
+        self.meaning_para_3 = meaning_para_3
+        self.meaning_para_4 = meaning_para_4
 
     @staticmethod
     def from_dict(source):
-        learning = Learning(source[u'content_id'], source[u'meaning_id'], source[u'question_id'])
 
-        if u'introduction_status' in source:
-            learning.introduction_status = source[u'introduction_status']
+        # cities_ref.document(u'BJ').set(
+        #     City(u'Beijing', None, u'China', True, 21500000, [u'hebei']).to_dict())
 
-        if u'story_status' in source:
-            learning.story_status = source[u'story_status']
+        learning = Learning(source[u'current_studying_content'][u'content_id'],
+                            source[u'current_studying_content'][u'last_watched_on'],
+                            source[u'current_studying_meaning'][u'meaning_id'],
+                            source[u'current_studying_meaning'][u'last_watched_on']
+                            )
 
-        if u'meaning_status' in source:
-            learning.meaning_status = source[u'meaning_status']
+        if u'para_1' in source[u'current_studying_content'][u'paragraphs']:
+            learning.content_para_1 = source[u'current_studying_content'][u'paragraphs'][u'para_1']
 
-        if u'example_status' in source:
-            learning.example_status = source[u'example_status']
+        if u'para_2' in source[u'current_studying_content'][u'paragraphs']:
+            learning.content_para_2 = source[u'current_studying_content'][u'paragraphs'][u'para_2']
+
+        if u'para_3' in source[u'current_studying_content'][u'paragraphs']:
+            learning.content_para_1 = source[u'current_studying_content'][u'paragraphs'][u'para_3']
+
+        if u'para_4' in source[u'current_studying_content'][u'paragraphs']:
+            learning.content_para_2 = source[u'current_studying_content'][u'paragraphs'][u'para_4']
+
+        if u'para_1' in source[u'current_studying_meaning'][u'paragraphs']:
+            learning.meaning_para_1 = source[u'current_studying_meaning'][u'paragraphs'][u'para_1']
+
+        if u'para_2' in source[u'current_studying_meaning'][u'paragraphs']:
+            learning.meaning_para_2 = source[u'current_studying_meaning'][u'paragraphs'][u'para_2']
+
+        if u'para_3' in source[u'current_studying_meaning'][u'paragraphs']:
+            learning.meaning_para_3 = source[u'current_studying_meaning'][u'paragraphs'][u'para_3']
+
+        if u'para_4' in source[u'current_studying_meaning'][u'paragraphs']:
+            learning.meaning_para_4 = source[u'current_studying_meaning'][u'paragraphs'][u'para_4']
 
         return learning
 
     def to_dict(self):
         # [START_EXCLUDE]
-        dest = {
-            u'content_id': self.content_id,
-            u'meaning_id': self.meaning_id,
-            u'question_id': self.question_id
+        db_dict = {
+            "current_studying_content": {
+                "last_watched_on": self.last_conent_watched_on,
+                "content_id":  self.content_id ,
+                "paragraphs": {
+                    "para_1": self.content_para_1,
+                    "para_2": self.content_para_2,
+                    "para_3": self.content_para_3,
+                    "para_4": self.content_para_4
+                }
+            },
+            "current_studying_meaning": {
+                "last_watched_on": self.last_meaning_watched_on,
+                "meaning_id": self.meaning_id,
+                "paragraphs": {
+                    "para_3": self.meaning_para_4,
+                    "para_4": self.meaning_para_3,
+                    "para_2": self.meaning_para_2,
+                    "para_1": self.meaning_para_1
+                }
+            }
         }
-
-        if self.introduction_status:
-            dest[u'capital'] = self.capital
-
-        if self.population:
-            dest[u'population'] = self.population
-
-        if self.regions:
-            dest[u'regions'] = self.regions
-
-        return dest
+        return db_dict
         # [END_EXCLUDE]
 
     def __repr__(self):
-        return(
-            u'City(name={}, country={}, population={}, capital={}, regions={})'
-            .format(self.name, self.country, self.population, self.capital,
-                    self.regions))
+        return (
+            u'Learning(user={}, content_id={}, last_conent_watched_on={}, content_para_1={}, content_para_2={},'
+            u'content_para_3={}, content_para_4={},'
+            u'meaning_id={}, last_meaning_watched_on={}, meaning_para_1={}, meaning_para_2={}, '
+            u'meaning_para_3={}, meaning_para_4={})'
+                .format(self.user, self.content_id, self.last_conent_watched_on, self.content_para_1,
+                        self.content_para_2, self.content_para_3, self.content_para_4,
+                        self.meaning_id, self.last_meaning_watched_on, self.meaning_para_1, self.meaning_para_2,
+                        self.meaning_para_3, self.meaning_para_4))
+
 # [END custom_class_def]
--- main.py
+++ main.py
@@ -1,162 +1,37 @@
 import json
-import logging
 import os
-from libraries.df_response_lib import *
 
-from flask import Flask,request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 from google.cloud import firestore
+
+from libraries.df_response_lib import *
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 log = app.logger
+
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
-@app.route('/', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-    res = None
-    logging.info('Request: ' + json.dumps(req, indent=4))
-
-    try:
-        action = req.get('queryResult').get('action')
-    except AttributeError:
-        return 'json error'
-
-
-    if action == 'get.session.status':
-        aog = actions_on_google_response()
-        fulfillmentText, current_content, current_meaning = sessionStatus(req)
-
-        aog_sr = aog.simple_response([
-            [fulfillmentText, fulfillmentText, False]
-        ])
-
-        aog_additional_resp = aog.simple_response([
-            ["Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'),
-             "Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'), False]
-        ])
-        basic_card = aog.basic_card(current_content.get(u'title') ,
-                                    current_meaning.get(u'title'),
-                                    current_content.get(u'paragraphs').get(u'para_1'),
-                                    # current_meaning.get(u'paragraphs').get(u'para_1').get(u'meaning_1'),
-                                    image=[current_content.get(u'images'), current_content.get(u'title')])
-
-        ff_response = fulfillment_response()
-        ff_text = ff_response.fulfillment_text(fulfillmentText)
-        ff_messages = ff_response.fulfillment_messages([aog_sr, basic_card, aog_additional_resp])
-
-        reply = ff_response.main_response(ff_text, ff_messages)
-
-
-    elif action == 'meaning.after.all':
-        aog = actions_on_google_response()
-        fulfillmentText, current_content, current_meaning = sessionStatus(req)
-
-        aog_sr = aog.simple_response([
-            [fulfillmentText, fulfillmentText, False]
-        ])
-
-        aog_additional_resp = aog.simple_response([
-            ["Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'),
-             "Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'), False]
-        ])
-
-        aog_additional_resp_2 = aog.simple_response([
-            ["Do you know the meaning of " + current_meaning.get(u'title'),
-             "Do you know the meaning of " + current_meaning.get(u'title'), False]
-        ])
-
-        # aog_sc = aog.suggestion_chips(current_content.get(u'items'))
-        # list_title, list_arr = get_content_list(current_content, current_meaning)
-        # list_select = aog.build_list_select(list_title, list_arr)
-
-        basic_card = aog.basic_card(current_content.get(u'title'),
-                                    current_meaning.get(u'title'),
-                                    current_content.get(u'paragraphs').get(u'para_1'),
-                                    # current_meaning.get(u'paragraphs').get(u'para_1').get(u'meaning_1'),
-                                    image=[current_content.get(u'images'), current_content.get(u'title')])
-
-        ff_response = fulfillment_response()
-        ff_text = ff_response.fulfillment_text(fulfillmentText)
-        ff_messages = ff_response.fulfillment_messages([aog_sr, basic_card, aog_additional_resp])
-
-        reply = ff_response.main_response(ff_text, ff_messages)
-
-        logging.info('meaning.after.all')
-    else:
-        logging.error('Unexpected action.')
-    return make_response(jsonify(reply))
-
-
-
-def sessionStatus(req):
-    logging.info(req.get('queryResult').get('parameters'))
-
-    result = req.get('queryResult')
-    parameters = result.get('parameters')
-    user = parameters.get('user-name')
-
-    try:
-        if not firebase_admin._apps:
-            db = setup_client()
-        db = firestore.client()
-        is_session_started, first_name = lookup_users(db, user)
-    except Exception as e:
-        logging.info(e)
-        exit(1)
-
-    if is_session_started == True:
-        speech = 'Ok ' + first_name + ', here is the latest idioms we are going to discuss in this session.'
-        current_content, current_meaning = get_content(db,user)
-        update_learning_for_user(db, user)
-
-    elif is_session_started == False:
-        speech = 'Ok ' + first_name + ', let me explain the objective, teaching and assessment methods.'
-        current_content, current_meaning = get_intro_content(db, user)
-    else:
-        speech = 'User ' + first_name + ' is not registered. Please register yourself, before starting this tutorials.'
-
-    logging.info('Response: %s', speech)
-    return speech, current_content, current_meaning
-
-def update_learning_for_user(db, user):
-
-    return ""
-
-def get_intro_content(db, user):
-    return ""
-
-def get_content_list(current_content, current_meaning):
-
-    list_title = current_content.get(u'title')
-
-    list_arr = [
-        [current_meaning.get(u'title'), current_meaning.get(u'paragraphs').get(u'para_1').get(u'meaning_1'),
-         [current_meaning.get(u'title'),[current_meaning.get(u'title')]],
-         [current_content.get(u'images'), current_content.get(u'title')]],
-        [current_meaning.get(u'title'), current_meaning.get(u'paragraphs').get(u'para_2').get(u'meaning_1'),
-         [current_meaning.get(u'title'), [current_meaning.get(u'title')]],
-         [current_content.get(u'images'), current_content.get(u'title')]]
-    ]
-
-    return list_title, list_arr
-
-def get_content(db, user):
-    learning = (db.collection(u'learnings').document(user).get()).to_dict()
-
-    content_dict = db.document(learning.get(u'current_studying_content').get(u'content_id').path).get().to_dict()
-    meaning_dict = db.document(learning.get(u'current_studying_meaning').get('meaning_id').path).get().to_dict()
-
-    return content_dict, meaning_dict
-
-def lookup_users(db, user):
-    user_ref = (db.collection(u'users').document(user)).get()
-    result = user_ref.to_dict()
-
-    return result.get(u'session_started'), result.get(u'first_name')
+from learnings import Learning
 
 
 def setup_client():
@@ -165,12 +40,180 @@ def setup_client():
     firebase_admin.initialize_app(cred, {
         'projectId': prject_id,
     })
-
     return firestore.client()
 
+
+try:
+    if not firebase_admin._apps:
+        db = setup_client()
+    db = firestore.client()
+except Exception as e:
+    log.info(e)
+    exit(1)
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    reply = {}
+    log.debug('Request: ' + json.dumps(req, indent=4))
+
+    try:
+        action = req.get('queryResult').get('action')
+
+        if action == 'get.session.status':
+            aog = actions_on_google_response()
+            fulfillment_text = session_status(req)
+
+            aog_sr = aog.simple_response([
+                [fulfillment_text, fulfillment_text, False]
+            ])
+
+            ff_response = fulfillment_response()
+            ff_text = ff_response.fulfillment_text(fulfillment_text)
+            ff_messages = ff_response.fulfillment_messages([aog_sr])
+
+            reply = ff_response.main_response(ff_text, ff_messages)
+            log.debug(json.dumps(reply, indent=4))
+
+        elif action == 'get.after.all.content':
+
+            log.debug(req.get('queryResult').get('parameters'))
+            user = req.get('queryResult').get('parameters').get('user-name')
+
+            reply = build_response_content(user)
+            log.debug(json.dumps(reply, indent=4))
+        else:
+            log.error('Unexpected action.')
+    except AttributeError:
+        log.error('json error')
+        return 'json error'
+    except Exception as e:
+        log.error(e)
+        return e
+
+    return make_response(jsonify(reply))
+
+
+def build_response_content(user):
+    """
+    buid response for the user request
+    :param user: user name from dialogflow
+    :return: response in the formate simple response & basic card
+    """
+
+    aog = actions_on_google_response()
+    current_content, current_meaning = get_content_for_user(user)
+
+    aog_sr = aog.simple_response([
+        ["Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'),
+         "Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'), True]
+    ])
+    basic_card = aog.basic_card(current_content.get(u'title'),
+                                current_meaning.get(u'title'),
+                                current_meaning.get(u'paragraphs').get(u'para_1').get(u'meaning_1'),
+                                image=[current_content.get(u'images'), current_content.get(u'title')])
+
+    ff_response = fulfillment_response()
+    ff_text = ff_response.fulfillment_text(aog_sr)
+    ff_messages = ff_response.fulfillment_messages([aog_sr, basic_card])
+    reply = ff_response.main_response(ff_text, ff_messages)
+    update_learning_for_user(user, current_content[u'content_id'], current_meaning[u'meaning_id'])
+    log.debug(json.dumps(reply, indent=4))
+    return reply
+
+
+def session_status(req):
+    """
+    Set the session for the user and fetch user info from db
+    :param req: request receive from dialogflow
+    :return: simple response in text
+    """
+
+    log.debug(req.get('queryResult').get('parameters'))
+
+    user = req.get('queryResult').get('parameters').get('user-name')
+
+    is_session_started, first_name = lookup_users(db, user)
+
+    if is_session_started == True:
+        speech = 'Ok ' + first_name + ', here is the latest idioms we are going to discuss in this session.'
+    elif is_session_started == False:
+        speech = 'Ok ' + first_name + ', let me explain the objective, teaching and assessment methods.'
+    else:
+        speech = 'User ' + first_name + ' is not registered. Please register yourself, before starting this tutorials.'
+
+    log.debug(json.dumps(speech, indent=4))
+    return speech
+
+
+def update_learning_for_user(user, content_id, meaning_id):
+    """
+    Update learing status for a user
+    :param user: user-name parameter as received from intent request
+    :param content_id: id for the content currently read by the user
+    :param meaning_id: id for the meaning currently read by the user
+    :return:
+    """
+    try:
+        db.collection(u'learnings').document(user).update(
+            Learning(user=user,
+                     content_id=db.document(content_id),
+                     last_content_watched_on=firestore.SERVER_TIMESTAMP,
+                     content_para_1=True,
+                     last_meaning_watched_on=firestore.SERVER_TIMESTAMP,
+                     meaning_id=db.document(meaning_id)
+                     ).to_dict())
+    except Exception as e:
+        log.error(e)
+        exit(1)
+
+    return 1
+
+
+def get_intro_content(db, user):
+    return ""
+
+
+def get_content_for_user(user):
+    """
+    Get current content & meaning in dict for the user
+    :param user: user-name parameter as received from intent request
+    :return: Content dictionary , Meaning dictionary
+    """
+    learning = (db.collection(u'learnings').document(user).get()).to_dict()
+    content_dict = db.document(learning.get(u'current_studying_content').get(u'content_id').path).get().to_dict()
+    meaning_dict = db.document(learning.get(u'current_studying_meaning').get('meaning_id').path).get().to_dict()
+    content_dict[u'content_id'] = learning.get(u'current_studying_content').get(u'content_id').path
+    meaning_dict[u'meaning_id'] = learning.get(u'current_studying_meaning').get('meaning_id').path
+    return content_dict, meaning_dict
+
+
+def lookup_users(db, user):
+    user_ref = (db.collection(u'users').document(user)).get()
+    result = user_ref.to_dict()
+
+    return result.get(u'session_started'), result.get(u'first_name')
+
+
+def get_content_list(current_content, current_meaning):
+    list_title = current_content.get(u'title')
+
+    list_arr = [
+        [current_meaning.get(u'title'), current_meaning.get(u'paragraphs').get(u'para_1').get(u'meaning_1'),
+         [current_meaning.get(u'title'), [current_meaning.get(u'title')]],
+         [current_content.get(u'images'), current_content.get(u'title')]],
+        [current_meaning.get(u'title'), current_meaning.get(u'paragraphs').get(u'para_2').get(u'meaning_1'),
+         [current_meaning.get(u'title'), [current_meaning.get(u'title')]],
+         [current_content.get(u'images'), current_content.get(u'title')]]
+    ]
+
+    return list_title, list_arr
+
+
 def generate_uuid(doc_ref, id_col):
-    return doc_ref.update({ id_col : firestore.Increment(1)})
+    return doc_ref.update({id_col: firestore.Increment(1)})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0' , port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
