8906349a27a9efc39a62d7eb683edf1ad08a06dc
SouravMallik <Sourav.Mallik@outlook.com>
2020-06-24 21:20:17 +0530

    intermitent commit with db & skill backup

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ dictConfig({
 app = Flask(__name__)
 log = app.logger
 
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -53,7 +52,7 @@ except Exception as e:
 
 
 @app.route('/', methods=['POST'])
-def webhook(request):
+def webhook():
     req = request.get_json(silent=True, force=True)
     reply = {}
     log.debug('Request: ' + json.dumps(req, indent=4))
@@ -67,7 +66,12 @@ def webhook(request):
             reply = session_status(user)
             log.debug(json.dumps(reply, indent=4))
         elif action == 'get.after.all.content':
-            reply = build_response_content(user)
+            idiom = req.get('queryResult').get('parameters').get('idioms')
+            reply = build_meaning_card(idiom, user)
+            log.debug(json.dumps(reply, indent=4))
+        elif action == 'get.crocodile.tears.content':
+            idiom = req.get('queryResult').get('parameters').get('idioms')
+            reply = build_meaning_card(idiom, user)
             log.debug(json.dumps(reply, indent=4))
         else:
             log.error('Unexpected action.')
@@ -81,33 +85,69 @@ def webhook(request):
     return make_response(jsonify(reply))
 
 
-def build_response_content(user, meaning_id = None):
+def build_content_list(user):
     """
-    buid response for the user request
-    :param meaning_id: optional till now
-    :param user: user name from dialogflow
+
+    :param idiom:
+    :return:
+    """
+    current_content, current_meaning = get_content_for_user(user)
+    meaning_list = get_meaning_list(current_content.get(u'content_id'))
+    aog = actions_on_google_response()
+
+    messag = f"Please go ahead and select the idiom you want to study."
+
+    aog_sr = aog.simple_response([
+        [messag, messag, False]
+    ])
+
+    list_arr = []
+    for meaning in meaning_list:
+        list_arr.append([meaning.get(u'title'), meaning.get(u'explanation').get(u'meaning'),
+                         [meaning.get(u'title'), [meaning.get(u'title')]],
+                         [meaning.get(u'image_url'), meaning.get(u'title')]])
+
+    list_select = aog.list_select(current_content[u'title'], list_arr)
+
+    ff_response = fulfillment_response()
+    ff_text = ff_response.fulfillment_text(messag)
+    ff_messages = ff_response.fulfillment_messages([aog_sr, list_select])
+
+    reply = ff_response.main_response(ff_text, ff_messages)
+
+    return reply
+
+
+def build_meaning_card(idiom,user=None):
+    """
+    buid card response with list for showing idiom meaning
+    :param idiom: idiom title for serching from mening collection
+    :param user: user name from dialogflow optional till now
     :return: response in the formate simple response & basic card
     """
 
     aog = actions_on_google_response()
-    current_content, current_meaning = get_content_for_user(user)
+    current_meaning = get_meaning_card(idiom)
 
-    aog_sr = aog.simple_response([
-        ["Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'),
-         "Here a small story for you. " + current_content.get(u'paragraphs').get(u'para_1'), True]
-    ])
-    basic_card = aog.basic_card(current_content.get(u'title'),
-                                current_meaning.get(u'title'),
-                                "Meaning: " + current_meaning.get(u'explanation').get(u'meaning') + os.linesep +
-                                "Origin: " + current_meaning.get(u'explanation').get(u'origin') + os.linesep +
-                                "Examples: " + current_meaning.get(u'explanation').get(u'examples'),
-                                image=[current_content.get(u'images'), current_content.get(u'title')])
+    message = f" {current_meaning.get(u'title')} means {current_meaning.get(u'explanation').get(u'meaning')}. \r\n"
+    if current_meaning.get(u'explanation').get(u'examples') != "":
+        message += f"For example {current_meaning.get(u'explanation').get(u'examples')}.\r\n"
+    if current_meaning.get(u'explanation').get(u'origin'):
+        message += f"Origin: {current_meaning.get(u'explanation').get(u'origin')}. \r\n"
+
+
+    aog_sr = aog.simple_response([[message, message, False]])
+
+    basic_card = aog.basic_card(current_meaning.get(u'title'),
+                                current_meaning.get(u'sub_title'),
+                                message,
+                                image=[current_meaning.get(u'image_url'), current_meaning.get(u'title')])
 
     ff_response = fulfillment_response()
     ff_text = ff_response.fulfillment_text(aog_sr)
     ff_messages = ff_response.fulfillment_messages([aog_sr, basic_card])
     reply = ff_response.main_response(ff_text, ff_messages)
-    update_learning_for_user(user, current_content[u'content_id'], current_meaning[u'meaning_id'])
+    # update_learning_for_user(user, current_content[u'content_id'], current_meaning[u'meaning_id'])
     log.debug(json.dumps(reply, indent=4))
     return reply
 
@@ -123,7 +163,8 @@ def session_status(user):
     if is_session_started == True:
         speech = 'Ok ' + first_name + ', here is the latest idioms we are going to discuss in this session.'
         current_content, current_meaning = get_content_for_user(user)
-        reply = get_content_list(speech, current_content, current_meaning, get_meaning_list(current_content[u'content_id']))
+        reply = get_content_list(speech, current_content, current_meaning,
+                                 get_meaning_list(current_content[u'content_id']))
     elif is_session_started == False:
         speech = 'Ok ' + first_name + ', let me explain the objective, teaching and assessment methods.'
         onboarding_user(user)
@@ -143,6 +184,7 @@ def session_status(user):
 
     log.debug(json.dumps(speech, indent=4))
     return reply
+
 
 def onboarding_user(user):
     """
@@ -165,6 +207,7 @@ def onboarding_user(user):
         return False
 
     return True
+
 
 def update_learning_for_user(user, content_id, meaning_id):
     """
@@ -207,6 +250,21 @@ def get_content_for_user(user):
     meaning_dict[u'meaning_id'] = learning.get(u'current_studying_meaning').get('meaning_id').path
     return content_dict, meaning_dict
 
+
+def get_meaning_card(meaning_title):
+    """
+    Get card response for a content using meaning title
+    :param meaning_title:
+    :return:
+    """
+    meaning_dict = {}
+    contents = db.collection_group(u'meaning').where(u'title', u'==', meaning_title).stream()
+    for contet in contents:
+        meaning_dict = contet.to_dict()
+
+    return meaning_dict
+
+
 def get_meaning_list(content_id):
     """
     Get list of meaning for a content id
@@ -219,6 +277,7 @@ def get_meaning_list(content_id):
         meaning_dict_list.append(meaning.to_dict())
 
     return meaning_dict_list
+
 
 def lookup_users(db, user):
     user_ref = (db.collection(u'users').document(user)).get()
@@ -238,16 +297,22 @@ def get_content_list(fulfillment_text, current_content, current_meaning, meaning
     """
 
     aog = actions_on_google_response()
+
+    messag = f"{fulfillment_text}. We will start with a short story, {current_content.get(u'sub_title')}. \n\r " \
+             f"{current_content.get(u'paragraphs').get(u'para_1')} \n\r " \
+             f"{current_content.get(u'paragraphs').get(u'para_4')} \n\r " \
+             f"Please go ahead and select the idiom you want to study."
+
     aog_sr = aog.simple_response([
-        [fulfillment_text, fulfillment_text, False]
+        [messag, messag, False]
     ])
 
     list_arr = []
 
     for meaning in meaning_list:
         list_arr.append([meaning.get(u'title'), meaning.get(u'explanation').get(u'meaning'),
-         [meaning.get(u'title'), [meaning.get(u'title')]],
-         [meaning.get(u'image_url'), meaning.get(u'title')]])
+                         [meaning.get(u'title'), [meaning.get(u'title')]],
+                         [meaning.get(u'image_url'), meaning.get(u'title')]])
 
     list_select = aog.list_select(current_content[u'title'], list_arr)
 
